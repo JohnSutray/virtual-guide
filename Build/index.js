0d065471ca42cc7077b490b6d77daa39a26d93fe
Andrey Zobkov <a.zobkov@itransition.com>
2020-12-04 07:03:23 +0300

    add build and heroku server

---
+++ Build/index.js
@@ -0,0 +1,6 @@
+﻿const express = require('express');
+express()
+  .use(express.static('./Build/'))
+  .use(express.static('./TemplateData/'))
+  .use(express.static('./'))
+  .listen(process.env.PORT || 5000);
